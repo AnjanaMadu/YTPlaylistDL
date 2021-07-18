831d51659a08e9587e3801dd69b0ad4e029e9da3
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-07-18 10:42:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,17 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-import pyrogram
+import pyrogram, os
 
 if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
-    app = pyrogram.Client(
-        "bot",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-    app.run()
+  plugins = dict(
+    root="plugins"
+  )
+  app = pyrogram.Client(
+    "bot",
+    bot_token=os.environ.get("TOKEN"),
+    api_id=int(os.environ.get("APP_ID")),
+    api_hash=os.environ.get("API_HASH"),
+    plugins=plugins
+  )
+  app.run()
