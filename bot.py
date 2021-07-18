6308ab62e23b2976588d7871f8db2a45a2a34739
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-07-18 10:38:41 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+import pyrogram
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = pyrogram.Client(
+        "bot",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    app.run()
