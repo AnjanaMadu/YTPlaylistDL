1842c1c2d55fbd3264dfbcf4b0269d6f6b9dbb15
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-07-18 21:02:14 +0530

    k

--- plugins/other.py
+++ plugins/other.py
@@ -4,13 +4,14 @@ import asyncio
 
 @Client.on_message(filters.command("start"))
 async def start_msg(client, message):
+
+	makup = [InlineKeyboardButton("Help", callback_data=f"helptxt_{k.message_id}"),
+					InlineKeyboardButton("About", callback_data=f"abouttxt_{k.message_id}")]
+
 	k = await client.send_message(message.chat.id, "...", reply_to_message_id=message.message_id)
 	await k.edit(
 		f"Hi {message.from_user.mention},If you need any help, Just click help button.\n\nProject by @Harp_Tech",
-		reply_markup=[
-			InlineKeyboardButton("Help", callback_data=f"helptxt_{k.message_id}"),
-			InlineKeyboardButton("About", callback_data=f"abouttxt_{k.message_id}")
-		]
+		reply_markup=makup
 	)
 
 @Client.on_callback_query()
